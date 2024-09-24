973761ab6e5103c189798d1a727e289e3464c5bb
NikitaDalal03 <159538476+NikitaDalal03@users.noreply.github.com>
2024-09-24 15:31:10 +0530

    random commit

--- Assets/Silantro/Common/Scripts/SilantroInput.cs
+++ Assets/Silantro/Common/Scripts/SilantroInput.cs
@@ -312,7 +312,7 @@ namespace Oyedoyin.Common
             {
                 // Collect and Send Inputs
                 if (m_collectiveLever != null) { _collectiveInput = m_collectiveLever.leverOutput; }
-                if (m_throttleLever != null) { _throttleInput = m_throttleLever.leverOutput; } else { _throttleInput = 0; }
+                if (m_throttleLever != null) { _throttleInput = m_throttleLever.leverOutput; } else { _throttleInput = -1; }
                 if (m_propPitchLever != null) { _propPitchInput = m_propPitchLever.leverOutput; }
                 if (m_mixtureLever != null) { _mixtureInput = m_mixtureLever.leverOutput; }
                 if (m_joystickLever != null) { _rawPitchInput = m_joystickLever.pitchOutput; }
